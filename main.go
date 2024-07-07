9b23d80488d546113b0fc760c9a56745356a4f53
BachErik <bacherik.info@gmail.com>
2024-07-07 09:47:20 +0200

    basic page generator

--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
-    "os"
     "fmt"
+    "io/ioutil"
+    "log"
+    "os"
+    "path/filepath"
 )
 
 func main() {
-    f, err := os.Create("index.html")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer f.Close()
+    // Define the directory paths
+    contentDir := "content"
+    templatesDir := "templates"
+    outputDir := "output"
 
-    _, err = f.WriteString("<html><body>Hello World!</body></html>")
+    // Read header and footer templates
+    header, err := ioutil.ReadFile(filepath.Join(templatesDir, "header.html"))
     if err != nil {
-        fmt.Println(err)
-        f.Close()
-        return
+        log.Fatalf("Error reading header: %v", err)
+    }
+    
+    footer, err := ioutil.ReadFile(filepath.Join(templatesDir, "footer.html"))
+    if err != nil {
+        log.Fatalf("Error reading footer: %v", err)
+    }
+
+    // Create output directory if it doesn't exist
+    if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+        os.Mkdir(outputDir, os.ModePerm)
+    }
+
+    // Read the content directory
+    files, err := ioutil.ReadDir(contentDir)
+    if err != nil {
+        log.Fatalf("Error reading content directory: %v", err)
+    }
+
+    for _, file := range files {
+        if filepath.Ext(file.Name()) == ".html" {
+            content, err := ioutil.ReadFile(filepath.Join(contentDir, file.Name()))
+            if err != nil {
+                log.Printf("Error reading content file %s: %v", file.Name(), err)
+                continue
+            }
+
+            outputFilePath := filepath.Join(outputDir, file.Name())
+            err = ioutil.WriteFile(outputFilePath, append(append(header, content...), footer...), 0644)
+            if err != nil {
+                log.Printf("Error writing output file %s: %v", outputFilePath, err)
+                continue
+            }
+
+            fmt.Printf("Generated %s\n", outputFilePath)
+        }
     }
 }
