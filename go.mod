728767fac8df8a9e5e65e2988202c503a49c55a4
BachErik <bacherik.info@gmail.com>
2024-07-11 19:29:43 +0200

    feat: add good OG image support for company

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module bacherik/killedby
 
 go 1.22.5
 
+require github.com/fogleman/gg v1.3.0
+
 require (
-	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
+	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
 	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -17,14 +17,21 @@ import (
 	"time"
 
 	"github.com/fogleman/gg"
+	"github.com/srwiley/oksvg"
+	"github.com/srwiley/rasterx"
+	// Import a package that can handle SVG to PNG conversion.
 )
 
-var githubUsername = os.Getenv("GITHUB_USERNAME")
-var githubRepository = os.Getenv("GITHUB_REPOSITORY")
-var cacheDir = "cache"
-var cacheDuration = 12 * time.Hour
+var (
+	githubUsername   = "bacherik"
+	githubRepository = "killedby.json"
+	cacheDir         = "cache"
+	cacheDuration    = 12 * time.Hour
+	companyConfig    = make(map[string]Company)
+	projectTypes     = make(map[string]string)
+	yearsProjects    []YearProjects
+)
 
-// Define the structs for JSON data
 type Project struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -49,10 +56,10 @@ type ProjectType struct {
 type BasePageData struct {
 	Title         string
 	Companies     map[string]string
-	OGTitle       string // Open Graph title
-	OGUrl         string // Open Graph URL
-	OGImage       string // Open Graph image URL
-	OGDescription string // Open Graph description
+	OGTitle       string
+	OGUrl         string
+	OGImage       string
+	OGDescription string
 }
 
 type YearProjects struct {
@@ -78,25 +85,19 @@ type ProjectPageData struct {
 }
 
 func main() {
-	// URLs for the JSON files and other initial setup
 	companiesURL := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/main/config/companies.json", githubUsername, githubRepository)
 	typesURL := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/main/config/types.json", githubUsername, githubRepository)
 
-	// Unmarshal company configs
-	companyConfig := make(map[string]Company)
 	err := fetchJSON(companiesURL, "companies.json", &companyConfig)
 	if err != nil {
 		log.Fatal("Error fetching company config:", err)
 	}
 
-	// Unmarshal project type configs
-	projectTypes := make(map[string]string)
 	err = fetchJSON(typesURL, "types.json", &projectTypes)
 	if err != nil {
 		log.Fatal("Error fetching project type config:", err)
 	}
 
-	// Fetch and unmarshal projects for each company
 	var allProjects []Project
 	for companyName := range companyConfig {
 		projectsURL := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/main/companies/%s.json", githubUsername, githubRepository, companyName)
@@ -107,34 +108,28 @@ func main() {
 			log.Fatalf("Error fetching projects for %s: %v", companyName, err)
 		}
 
-		// Add projects to the company in the map
 		company := companyConfig[companyName]
 		company.Projects = projects
 		companyConfig[companyName] = company
 		allProjects = append(allProjects, projects...)
 	}
 
-	// Group projects by year
 	projectYearMap := make(map[string][]Project)
 	for _, project := range allProjects {
-		year := strings.Split(project.DateClose, "-")[0] // Assuming DateClose is in YYYY-MM-DD format
+		year := strings.Split(project.DateClose, "-")[0]
 		projectYearMap[year] = append(projectYearMap[year], project)
 	}
 
-	// Sort projects within each year
 	for _, projects := range projectYearMap {
 		sort.Slice(projects, func(i, j int) bool {
 			return projects[i].DateClose > projects[j].DateClose
 		})
 	}
 
-	// Convert the map to a slice for ordered processing in templates
-	var yearsProjects []YearProjects
 	for year, projects := range projectYearMap {
 		yearsProjects = append(yearsProjects, YearProjects{Year: year, Projects: projects})
 	}
 
-	// Sort years to display them in order
 	sort.Slice(yearsProjects, func(i, j int) bool {
 		return yearsProjects[i].Year > yearsProjects[j].Year
 	})
@@ -162,8 +157,17 @@ func main() {
 		log.Fatalf("Error parsing templates: %v", err)
 	}
 
-	// Handlers
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", indexHandler(tmpl))
+	http.HandleFunc("/company/", companyHandler(tmpl))
+	http.HandleFunc("/project/", projectHandler(tmpl))
+	http.HandleFunc("/og/", ogImageHandler)
+
+	fmt.Println("Starting server at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func indexHandler(tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		scheme := "http"
 		if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
 			scheme = "https"
@@ -191,9 +195,11 @@ func main() {
 			log.Printf("Error executing template: %v", err)
 			http.Error(w, "Internal Server Error", 500)
 		}
-	})
+	}
+}
 
-	http.HandleFunc("/company/", func(w http.ResponseWriter, r *http.Request) {
+func companyHandler(tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		scheme := "http"
 		if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
 			scheme = "https"
@@ -230,7 +236,7 @@ func main() {
 				// Adding Open Graph properties
 				OGTitle:       "Killed by - " + companyName,
 				OGUrl:         getFullURL(r),
-				OGImage:       scheme + "://" + r.Host + "/og/" + r.RequestURI,
+				OGImage:       scheme + "://" + r.Host + "/og/company/" + companyName,
 				OGDescription: company.Description,
 			},
 			YearsProjects: yearsProjects, // Use grouped projects by year
@@ -246,9 +252,11 @@ func main() {
 			log.Printf("Error executing template: %v", err)
 			http.Error(w, "Internal Server Error", 500)
 		}
-	})
+	}
+}
 
-	http.HandleFunc("/project/", func(w http.ResponseWriter, r *http.Request) {
+func projectHandler(tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		scheme := "http"
 		if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
 			scheme = "https"
@@ -286,9 +294,9 @@ func main() {
 				Title:     projectName,
 				Companies: make(map[string]string),
 				// Adding Open Graph properties
-				OGTitle:       projectName + "is a " + project.Type + " being killed by " + companyName + " in " + strings.Split(project.DateClose, "-")[0],
+				OGTitle:       projectName + " is a " + project.Type + " being killed by " + companyName + " in " + strings.Split(project.DateClose, "-")[0],
 				OGUrl:         getFullURL(r),
-				OGImage:       scheme + "://" + r.Host + "/og/" + r.RequestURI,
+				OGImage:       scheme + "://" + r.Host + "/og/project/" + companyName + "/" + projectName,
 				OGDescription: project.Description,
 			},
 			Project: project,
@@ -303,14 +311,20 @@ func main() {
 			log.Printf("Error executing template: %v", err)
 			http.Error(w, "Internal Server Error", 500)
 		}
-	})
+	}
+}
 
-	fmt.Println("Starting server at :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+// getFullURL returns the full URL from the given http.Request object.
+func getFullURL(r *http.Request) string {
+	// Check if the request is made over HTTPS
+	scheme := "http"
+	if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
+		scheme = "https"
+	}
+	return fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)
 }
 
 func fetchJSON(url, cacheFileName string, v interface{}) error {
-	// Create cache directory if it doesn't exist
 	if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
 		err := os.Mkdir(cacheDir, 0755)
 		if err != nil {
@@ -319,11 +333,8 @@ func fetchJSON(url, cacheFileName string, v interface{}) error {
 	}
 
 	cacheFilePath := filepath.Join(cacheDir, cacheFileName)
-
-	// Check if the cached file exists and is still valid
 	if info, err := os.Stat(cacheFilePath); err == nil {
 		if time.Since(info.ModTime()) < cacheDuration {
-			// Read from the cached file
 			bytes, err := os.ReadFile(cacheFilePath)
 			if err != nil {
 				return err
@@ -332,9 +343,6 @@ func fetchJSON(url, cacheFileName string, v interface{}) error {
 		}
 	}
 
-	fmt.Print("Fetching ", url, "... ")
-
-	// Fetch from the URL
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
@@ -350,13 +358,11 @@ func fetchJSON(url, cacheFileName string, v interface{}) error {
 		return err
 	}
 
-	// Unmarshal the JSON data
 	err = json.Unmarshal(bytes, v)
 	if err != nil {
 		return err
 	}
 
-	// Write the data to the cache file
 	err = os.WriteFile(cacheFilePath, bytes, 0644)
 	if err != nil {
 		return err
@@ -365,9 +371,8 @@ func fetchJSON(url, cacheFileName string, v interface{}) error {
 	return nil
 }
 
-// isFutureDate checks if the given date string is in the future.
 func isFutureDate(dateStr string) bool {
-	layout := "2006-01-02" // Adjust this layout as necessary to match the date format
+	layout := "2006-01-02"
 	date, err := time.Parse(layout, dateStr)
 	if err != nil {
 		log.Printf("Error parsing date: %v", err)
@@ -376,57 +381,98 @@ func isFutureDate(dateStr string) bool {
 	return date.After(time.Now())
 }
 
-// getFullURL returns the full URL from the given http.Request object.
-func getFullURL(r *http.Request) string {
-	// Check if the request is made over HTTPS
-	scheme := "http"
-	if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
-		scheme = "https"
+func downloadImage(url string) (image.Image, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
 	}
-	return fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)
+	defer resp.Body.Close()
+
+	if strings.HasSuffix(url, ".svg") {
+		// Parse SVG data
+		icon, err := oksvg.ReadIconStream(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		w, h := int(icon.ViewBox.W), int(icon.ViewBox.H)
+		img := image.NewRGBA(image.Rect(0, 0, w, h))
+		scanner := rasterx.NewScannerGV(w, h, img, img.Bounds())
+		raster := rasterx.NewDasher(w, h, scanner)
+
+		icon.SetTarget(0, 0, float64(w), float64(h))
+		icon.Draw(raster, 1.0)
+
+		return img, nil
+	} else {
+		// Handle other image formats
+		img, _, err := image.Decode(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return img, nil
+	}
 }
 
-func generateImage(text string) image.Image {
+func generateImage(text string, logoURL string, description string) image.Image {
 	const W = 1200
 	const H = 630
 	dc := gg.NewContext(W, H)
 	dc.SetRGB(0, 0, 0) // Set background color
 	dc.Clear()
-	dc.SetRGB(1, 1, 1)                       // Set text color
-	dc.LoadFontFace("/path/to/font.ttf", 48) // Load your font
-	dc.DrawStringAnchored(text, float64(W)/2, float64(H)/2, 0.5, 0.5)
+	dc.SetRGB(1, 1, 1) // Set text color
+
+	fontPath := "./classic-stroke/ClassicStroke-Texture.ttf"
+	if err := dc.LoadFontFace(fontPath, 48); err != nil {
+		log.Fatalf("Error loading font: %v", err)
+	}
+
+	dc.DrawStringAnchored(text, float64(W)/2, float64(H)/4, 0.5, 0.5)
+
+	dc.LoadFontFace(fontPath, 24)
+	dc.DrawStringWrapped(description, float64(W)/2, float64(H)/2, 0.5, 0.5, float64(W)-40, 1.5, gg.AlignCenter)
+
+	if logoURL != "" {
+		logo, err := downloadImage(logoURL)
+		if err == nil {
+			dc.DrawImageAnchored(logo, W/2, 3*H/4, 0.5, 0.5)
+		} else {
+			log.Printf("Error downloading logo: %v", err)
+		}
+	}
+
 	return dc.Image()
 }
 
 func ogImageHandler(w http.ResponseWriter, r *http.Request) {
-	// Extract the path after /og/
 	path := strings.TrimPrefix(r.URL.Path, "/og/")
 	if path == "" {
 		http.Error(w, "Invalid request", http.StatusBadRequest)
 		return
 	}
 
-	// Decide what text to display based on the 'path'
-	text := generateTextForOGImage(path)
+	text, logoURL, description := generateTextForOGImage(path)
 
-	// Generate the image
-	img := generateImage(text) // Implement this function to generate an image based on the text
+	img := generateImage(text, logoURL, description)
 
-	// Set the header and write the image
 	w.Header().Set("Content-Type", "image/png")
 	if err := png.Encode(w, img); err != nil {
 		http.Error(w, "Failed to encode image", http.StatusInternalServerError)
 	}
 }
 
-func generateTextForOGImage(path string) string {
-	// Simple routing logic based on path
+func generateTextForOGImage(path string) (string, string, string) {
 	if strings.HasPrefix(path, "company/") {
-		return "Information about " + strings.TrimPrefix(path, "company/")
+		companyName := strings.TrimPrefix(path, "company/")
+		company, ok := companyConfig[companyName]
+		if ok {
+			return "Killed by - " + companyName, company.Logo, company.Description
+		}
+		return "Company Not Found", "", ""
 	} else if strings.HasPrefix(path, "project/") {
-		return "Details of project " + strings.TrimPrefix(path, "project/")
+		return "Details of project " + strings.TrimPrefix(path, "project/"), "", ""
 	} else if strings.HasPrefix(path, "home/") {
-		return "Killed by - Explore discontinued projects"
+		return "Killed by - Explore discontinued projects", "", ""
 	}
-	return "Killed by - Explore discontinued projects"
+	return "Killed by - Explore discontinued projects", "", ""
 }
